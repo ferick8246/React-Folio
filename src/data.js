92919f4815cb92af28667ff715fc4726b151d1f3
Erick Flore <ferick8246@gmail.com>
2021-07-23 00:50:08 -0500

    data.js

---
+++ src/data.js
@@ -0,0 +1,80 @@
+export const projects = [
+  {
+    title: "WeatherApp",
+    subtitle: "JavaScript, HTML, and CSS",
+    description:
+      "A simple secure password generator, highlighting vanilla JavaScript skills.",
+    image: "./images/weather.png",
+    link: "https://ferick8246.github.io/Weather-Update/",
+  },
+  {
+    title: "BatQuiz",
+    subtitle: "JavaScript, HTML, and CSS",
+    description:
+      "A fun, timed JavaScript Quiz, with sound and point system, highlighting vanilla JavaScript skills.",
+    image: "./images/Batquiz.PNG",
+    link: "https://ferick8246.github.io/Quiz-Code/",
+  },
+  {
+    title: "Employee Tracker",
+    subtitle: "JavaScript, HTML, and CSS",
+    description:
+      "This was a simple HTML/CSS/vanilla JavaScript project that uses the local storage to save data.",
+    image: "./images/Employee.PNG",
+    link: "https://github.com/ferick8246/Emplyee-Tracker-9000",
+  },
+  {
+    title: "Note Tacker",
+    subtitle: "JavaScript, HTML, and Node",
+    description:
+      "A weather dashboard using a server-side API to display weather forecasts.",
+    image: "./images/NoteTaker.PNG",
+    link: "https://woodys-notetaker.herokuapp.com/",
+  },
+  {
+    title: "Fitness Tracker",
+    subtitle: "JavaScript, HTML, and CSS",
+    description:
+      "A fun project highlighting GET requests to server-side APIs.",
+    image: "./images/Fitness.PNG",
+    link: "https://fitness-trakerz.herokuapp.com/exercise?id=60e7bfeea7d2790015381b0d",
+  },
+  {
+    title: "Budget Tracker",
+    subtitle: "Node.js",
+    description:
+      "A Node.js program to create a README using your terminal.",
+    image: "./images/Budget.PNG",
+    link: "https://budgetracker2.herokuapp.com/",
+  },
+ 
+
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "HTML and CSS",
+  "Node",
+  "GraphQL",
+  "MySQL",
+  "ArcMap",
+  "APIs",
+];
